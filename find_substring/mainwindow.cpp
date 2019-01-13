a914494e14e693b7390c83bb5acb484bdd7ee8a9
CaelmBleidd <alex.menshutin99@gmail.com>
2019-01-13 21:44:25 +0300

    Visual changes

--- find_substring/mainwindow.cpp
+++ find_substring/mainwindow.cpp
@@ -4,45 +4,76 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
 
     ui->setupUi(this);
-
     connect(ui->actionIndexing_directory, &QPushButton::clicked, this, &MainWindow::indexing);
     connect(ui->actionGo_home, &QPushButton::clicked, this, &MainWindow::go_home);
     connect(ui->actionGo_back, &QPushButton::clicked, this, &MainWindow::go_back);
     connect(ui->actionCancel, &QPushButton::clicked, this, &MainWindow::cancel);
-    connect(ui->actionShow_directory, &QPushButton::clicked, this, &MainWindow::show_directory);
-    connect(ui->actionChoose_directory, &QPushButton::clicked, this, &MainWindow::choose_directory);
+    connect(ui->actionChoose_directory, &QPushButton::clicked, this, &MainWindow::select_directory);
+
+    ui->treeWidget->header()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+    ui->treeWidget->header()->setSectionResizeMode(1, QHeaderView::Stretch);
+
+    show_directory(QDir::homePath());
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow() {}
+
+void MainWindow::select_directory() {
+    QString dir = QFileDialog::getExistingDirectory(this, "Select directory", QDir::homePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    if (!dir.isEmpty()) {
+        MainWindow::show_directory(dir);
+    } else {
+        QMessageBox warning;
+        warning.setText("Empty path");
+        warning.setEscapeButton(QMessageBox::Ok);
+        warning.exec();
+    }
+}
+
+
+void MainWindow::show_directory(QString const &dir) {
+    ui->treeWidget->clear();
+
+    QDirIterator iter(dir, QDir::NoDot | QDir::AllEntries);
+    QDir::setCurrent(dir);
+
+    setWindowTitle(QDir::currentPath());
+
+    while (iter.hasNext()) {
+        iter.next();
+        QTreeWidgetItem *item = new QTreeWidgetItem(ui->treeWidget);
+        set_data(item, iter.filePath());
+    }
+
+    ui->treeWidget->sortItems(0, Qt::SortOrder::AscendingOrder);
+}
+
+
+void MainWindow::set_data(QTreeWidgetItem *item, QString const &path) {
+    QFileInfo file(path);
+
+    item->setTextColor(0, Qt::black);
+    item->setTextColor(1, Qt::black);
+
+    item->setText(0, file.fileName());
+    item->setText(1, file.filePath());
+}
+
+void MainWindow::indexing() {
 
 }
 
-void MainWindow::show_directory()
-{
 
+void MainWindow::go_home() {
+    QDir::setCurrent(QDir::homePath());
+    show_directory(QDir::homePath());
 }
 
-void MainWindow::indexing()
-{
-
+void MainWindow::go_back() {
+    QDir::setCurrent("..");
+    show_directory(QDir::currentPath());
 }
 
-void MainWindow::choose_directory()
-{
-
-}
-
-void MainWindow::go_home()
-{
-
-}
-
-void MainWindow::go_back()
-{
-
-}
-
-void MainWindow::cancel()
-{
+void MainWindow::cancel() {
 
 }
--- find_substring/mainwindow.h
+++ find_substring/mainwindow.h
@@ -2,7 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
 #include <memory>
+#include <QDirIterator>
+#include <QTreeWidgetItem>
+#include <QCommonStyle>
+#include <QDesktopWidget>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QDateTime>
+#include <QFileSystemModel>
+#include <QCryptographicHash>
+#include <QIODevice>
+#include <QMainWindow>
+#include <memory>
+#include <QDir>
+#include <QWidgetItem>
+#include <QMap>
+#include <QTreeWidget>
+
 
 namespace Ui {
 class MainWindow;
@@ -16,8 +35,25 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+
 private:
     std::unique_ptr<Ui::MainWindow> ui;
+    QDir actual_directory;
+    void set_data(QTreeWidgetItem*, QString const&);
+    void show_directory(QString const&);
+
+
+private slots:
+    void select_directory();
+    void indexing();
+    void go_home();
+    void go_back();
+    void cancel();
+
+
 };
+
+
 
 #endif // MAINWINDOW_H
